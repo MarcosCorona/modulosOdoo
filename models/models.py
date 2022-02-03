506cce4162dfcb6356dd7d8bf27e7834302d5a03
Marcos <Marcoscoronadionisio@gmail.com>
2022-02-03 15:54:33 +0100

    Crear modelo para el modulo

--- models/models.py
+++ models/models.py
@@ -16,3 +16,34 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+from odoo import models ,fields, api
+
+class departamento(models.Model):
+	_name = 'proyectos.departamento'
+	_description = 'Define los atributos de un departamento'
+
+	#atributos
+	nombreDpto = fields.Char(string='Nombre departamento', required=True)
+
+class empleado(models.model):
+	_name = 'proyectos.empleado'
+	_description = 'Define los atributos de un empleado'
+	 
+	#atributos
+	dniEmpleado = fields.Char(string = 'Dni ',requierd = True)
+	nombreEmpleado = fields.Char(string = 'Nombre ',requierd = True)
+	fechaNacimiento = fields.Date(string = 'Fecha Nacimiento',requierd = True, default = fields.Date.today())
+	direccionEmpleado = fields.Char(string = 'Direccion ',requierd = True)
+	telefonoEmpleado = fields.Char(string = 'Telefono ',requierd = True)
+
+class proyecto(models.Model)
+	_name = 'proyectos.proyecto'
+	_description = 'Define los atributos de un proyecto'
+
+	#atributos
+	nombreProyecto = fields.Char(string = 'Nombre proyecto',required = True)
+	tipoProyecto = fields.Selection(string = 'Tipo proyecto',selection =[('f','Front-End'),('b','Back-end')],help='Tipo de proyecto al que está destinado')
+	ciudadProyecto = fields.Char(string='Ciudad')
+	descripcionProyecto = fields.Text(string ='Descripcion proyecto')
+
+	
