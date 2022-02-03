6dce7415ecaab0f6c73b1bc35795924e50cdfdf8
MarcosCorona <91139292+MarcosCorona@users.noreply.github.com>
2022-02-03 21:40:50 +0100

    fin

--- models/models.py
+++ models/models.py
@@ -38,7 +38,7 @@ class empleado(models.model):
 	direccionEmpleado = fields.Char(string = 'Direccion ',required = True)
 	telefonoEmpleado = fields.Char(string = 'Telefono ',required = True)
 
-class proyecto(models.Model)
+class proyecto(models.Model):
 	_name = 'proyectos.proyecto'
 	_description = 'Define los atributos de un proyecto'
 
