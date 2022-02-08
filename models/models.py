c1fc48526229c8b7153002c4ef7105149868dffa
Marcos <Marcoscoronadionisio@gmail.com>
2022-02-08 19:21:51 +0100

    prueba123

--- models/models.py
+++ models/models.py
@@ -18,34 +18,50 @@
 #             record.value2 = float(record.value) / 100
 
 #Comentario
-from odoo import models ,fields, api
+from odoo import models, fields, api
 
 class departamento(models.Model):
-	_name = 'proyectos.departamento'
-	_description = 'Define los atributos de un departamento'
-
-	#atributos
-	nombreDpto = fields.Char(string='Nombre departamento', required=True)
+    _name = 'proyectos.departamento'
+    _description = 'Define los atributos de un departamento'
+    
+    nombreDepartamento= fields.Char(string='Nombre departamento', required=True)
+    
+    #Relaciones entre tablas
+    #Comentario de prueba
+    empleado_id = fields.One2many('proyectos.empleado','departamento_id' ,string='Departamento')
 
 class empleado(models.Model):
-	_name = 'proyectos.empleado'
-	_description = 'Define los atributos de un empleado'
-	 
-	#atributos
-	dniEmpleado = fields.Char(string = 'Dni ',required = True)
-	nombreEmpleado = fields.Char(string = 'Nombre ',required = True)
-	fechaNacimiento = fields.Date(string = 'Fecha Nacimiento',required = True, default = fields.Date.today())
-	direccionEmpleado = fields.Char(string = 'Direccion ',required = True)
-	telefonoEmpleado = fields.Char(string = 'Telefono ',required = True)
+    _name = 'proyectos.empleado'
+    _description = 'Atributos para un empleado'
+    #atributos
+    dniEmpleado = fields.Char(string='DNI', requiered=True)
+    nombreEmpleado = fields.Char(string='Nombre', requiered=True)
+    fechaNacimiento = fields.Date(string='Fecha nacimiento', requiered=True, default = fields.date.today())
+    direccionEmpleado = fields.Char(string='Dirección', requiered=True)
+    telefonoEmpleado = fields.Char(string='Telefono', requiered=True)
+    edad = fields.Integer('Edad', compute='getEdad')
+
+    @api.depends('fechaNacimiento')
+    def getEdad(self):
+        for empleado in self:
+            empleado.edad = 0
+    
+    #relaciones entre tablas
+    departamento_id = fields.Many2one('proyectos.departamento', string='Empleados')
+    proyecto_ids = fields.Many2many('proyectos.proyecto', string='Proyectos')
 
 class proyecto(models.Model):
-	_name = 'proyectos.proyecto'
-	_description = 'Define los atributos de un proyecto'
+    _name = 'proyectos.proyecto'
+    _description = 'Atributos de un proyecto'
+    
+    nombreProyecto = fields.Char(string='Nombre proyecto', requiered=True)
+    tipoProyecto = fields.Selection(string='Tipo de proyecto', selection=[('f','Front-End'),('b','Back-End')], requiered=True, help='Indica el tipo de proyecto')
+    ciudadProyecto = fields.Char(string='Ciudad')
+    decripcionProyecto = fields.Text(string='Descripcion del proyecto', help='Indica una descripción del proyecto')
+    fechaInicio = fields.Date(string='Fecha Inicio', required=True)
+    fechaFin = fields.Date(string='Fecha Final', required=True)
+    
+    #Relación entre tablas
+    empleado_ids = fields.Many2many('proyectos.empleado', string='Empleados')
 
-	#atributos
-	nombreProyecto = fields.Char(string = 'Nombre proyecto',required = True)
-	tipoProyecto = fields.Selection(string = 'Tipo proyecto',selection =[('f','Front-End'),('b','Back-end')],help='Tipo de proyecto al que est� destinado')
-	ciudadProyecto = fields.Char(string='Ciudad',required = True)
-	descripcionProyecto = fields.Text(string ='Descripcion proyecto',required = True)
 
-	
