4fcf74fc553b48e8fd3df4a507056110c7072cb7
Marcos <Marcoscoronadionisio@gmail.com>
2022-02-14 16:29:35 +0100

    asd

--- models/models.py
+++ models/models.py
@@ -73,7 +73,7 @@ class proyecto(models.Model):
             if (dias < 0):
                 raise exceptions.ValidationError("La fecha no puede ser anterior a hoy")
 
-    @api.depends('fechaNacimiento')
+    @api.constrains('fechaNacimiento')
       def _getEdad(self):
         hoy = date.today()
         for empleado in self:
